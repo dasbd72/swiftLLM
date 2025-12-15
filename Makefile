2bc858c3cb3e325d71c4eb0971bba2cb87bd9cef
saohsuan <twbd723@gmail.com>
2025-12-15 10:56:14 +0800

    chore: formatted c source

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+C_CXX_SOURCES := $(shell find csrc -name *.c -or -name *.cpp -or -name *.cc -or -name *.h -or -name *.hpp)
+
 lint:
 	isort --line-length 80 --profile google .
 	pyink --line-length 80 --preview --unstable .
 	flake8 --ignore=E203,E501,W503 --exclude venv
+	clang-format -i $(C_CXX_SOURCES)
--- csrc/src/block_swapping.h
+++ csrc/src/block_swapping.h
@@ -2,16 +2,12 @@
 
 #include <torch/extension.h>
 
-#include <vector>
 #include <cstdint>
+#include <vector>
 
-void swap_blocks(
-	const std::vector<int64_t> &source_block_ids,
-	const std::vector<int64_t> &target_block_ids,
-	const bool is_swap_in,
+void swap_blocks(const std::vector<int64_t> &source_block_ids,
+                 const std::vector<int64_t> &target_block_ids,
+                 const bool is_swap_in,
 
-	torch::Tensor k_cache,
-	torch::Tensor v_cache,
-	torch::Tensor k_swap,
-	torch::Tensor v_swap
-);
+                 torch::Tensor k_cache, torch::Tensor v_cache,
+                 torch::Tensor k_swap, torch::Tensor v_swap);
